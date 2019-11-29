a1006a0641a6bd9d9be6b7dd619d5ee4c110ca80
Mate Farkas <mate.farkas@sch.hu>
2019-11-29 16:53:52 +0000

    Initial commit

---
+++ lib/kubectl_eyaml.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'kubectl_eyaml/version'
+
+module KubectlEyaml
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kubectl_eyaml/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module KubectlEyaml
+  VERSION = '1.0.0'
+end
